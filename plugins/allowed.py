88be98f1202d9f71d31068d3fd76f657d1e3681e
MrWhite <68634413+neelshah55@users.noreply.github.com>
2024-01-08 23:10:21 +0530

    Update allowed.py

--- plugins/allowed.py
+++ plugins/allowed.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from info import PRIVATE_BOT, OWNER
+from info import PRIVATE_BOT, OWNER, UPDATES_CHANNEL, DEV_LINK
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
@@ -13,8 +13,8 @@ allowed_user = filters.create(owner)
 @Client.on_message(filters.private & allowed_user & filters.incoming)
 async def not_owner(bot, message):
     btn = [[
-        InlineKeyboardButton('Repo', url='https://github.com/Hansaka-Anuhas/ForwardBot')
+        InlineKeyboardButton('Updates Channel', url=UPDATES_CHANNEL)
     ],[
-        InlineKeyboardButton('Developer', url='https://t.me/Hansaka_Anuhas')
+        InlineKeyboardButton('Contact', url=DEV_LINK)
     ]]
     await message.reply("You can't access this bot, Create your own bot.", reply_markup=InlineKeyboardMarkup(btn))
